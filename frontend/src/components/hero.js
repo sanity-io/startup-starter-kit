5bb8af7efa51d7683756214316dac90bcfe309ca
Marius Lundgård <studio@mariuslundgard.com>
2018-11-30 11:53:55 +0100

    Add Hero component

---
+++ frontend/src/components/hero.js
@@ -0,0 +1,16 @@
+import { Link } from 'gatsby'
+import React from 'react'
+
+import styles from './hero.module.css'
+
+const Hero = ({ headline, intro }) => (
+  <div className={styles.root}>
+    <h1 className={styles.headline}>{headline}</h1>
+    <div>{intro}</div>
+    <div>
+      <Link to="/start/">Get started</Link>
+    </div>
+  </div>
+)
+
+export default Hero
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import Hero from '../components/hero'
 import Layout from '../components/layout'
-import Image from '../components/image'
+// import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-      <Image />
-    </div>
+    <Hero
+      headline="My awesome headline that explains the whole shebang"
+      intro={
+        <>
+          <p>My awesome intro that explains the whole shebang.</p>
+        </>
+      }
+    />
+
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
