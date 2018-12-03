b55ee31169288b4a03f52e94ecf06b93a4613aa0
Knut Melvær <knut.melvaer@gmail.com>
2018-12-03 08:13:26 +0100

    Add post document type

--- backend/schemas/documents/index.js
+++ backend/schemas/documents/index.js
@@ -1,6 +1,7 @@
 export { default as agent } from './agent'
 export { default as companyInfo } from './companyInfo'
+export { default as fulfillment } from './fulfillment'
 export { default as intent } from './intent'
 export { default as page } from './page'
+export { default as post } from './post'
 export { default as route } from './route'
-export { default as fulfillment } from './fulfillment'
---
+++ backend/schemas/documents/post.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'post',
+  type: 'object',
+  title: 'Post',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'title',
+      options: []
+    },
+
+  ]
+}
