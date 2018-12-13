e77829a2dd21eca8dbc97928f134fbf7e8c3ca1f
Knut Melvær <knut.melvaer@gmail.com>
2018-12-13 14:27:49 +0100

    fix git caps :bug:

--- frontend/src/components/SiteNav.js
+++ frontend/src/components/SiteNav.js
@@ -1,7 +1,7 @@
 import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
-import Icon from "./Icon"
+
 import { fontScale, media, padding, width, zIndex } from "./vars"
 
 const Root = styled.nav`
